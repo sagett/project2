89f09ffbcf609bfb93c2573ee6754f7e8882b49f
tiam <tiam_taheri@yahoo.de>
2013-10-14 23:34:53 -0400

    second commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$.ajax({ 
+    url: 'https://api.imgur.com/3/image',
+    headers: {
+        'Authorization': 'Client-ID 699913a195a439d'
+    },
+    type: 'POST',
+    data: {
+        'image': 'helloworld.jpg'
+    },
+    success: function() { console.log('cool'); }
+});
