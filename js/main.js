3ccbf1fcda20650b8efa91b718edb444b7d03e05
tiam <tiam_taheri@yahoo.de>
2013-10-16 10:38:02 -0400

    161013_a

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
-
-
-function updating () {
-	
-
-}
-
-
-
-
+/*
 $.ajax({ 
-    url: 'https://api.imgur.com/3/image',
+    url:'https://api.imgur.com/3/gallery.json',
     headers: {
         'Authorization': 'Client-ID 699913a195a439d'
     },
     type: 'POST',
     data: {
-        'image': 'helloworld.jpg'
+        'Authorization': 'Client-ID 699913a195a439d'
     },
-    success: function() { console.log('cool'); }
+    success: function(data) { 
+    console.log(data); 
+    }
+});
+*/
+
+$.ajax({ 
+    url: 'https://api.imgur.com/3/gallery.json',
+    headers: {
+        'Authorization': 'Client-ID 699913a195a439d'
+    },
+    type: 'GET',
+    success: function(data) { 
+    	console.log(data.data.length);
+    	console.log(data.data[0].link); 
+    var Imgur = function() {
+	    this.items = [];
+	    this.page=0;
+	    };
+    
+   Imgur.Page = function() {
+	   var url
+	   
+	   
+   }
+   
+   
+    }
 });
