4c34dd06c8e5de010da5c5145e5657077891d315
tiam <tiam_taheri@yahoo.de>
2013-10-22 11:40:55 -0400

    221013_b

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ function displayImg(URL){
 		}
 		displayImg(URL);
 	
+
    }
  }
    
@@ -37,18 +38,80 @@ function displayImg(URL){
 $('.container').jscroll();
 	
 	
-	function storeGif(URL){
+	localStorage.setItem("gifs", JSON.stringify(imageArray));
+	JSON.parse(localStorage.getItem("gifs"));
 
 
-    // make the array a string
-    imageArray = JSON.stringify(URL);
-    console.log('json: ' + URL);
-
-    // store the string
-    localStorage.imageArray = imageArray;
-
+	
+	
+	var storageFiles = JSON.parse(localStorage.getItem("storageFiles")) || {},
+    imageID = document.getElementById("a"),
+    storageFilesDate = storageFiles.date,
+    date = new Date(),
+    todaysDate = (date.getMonth() + 1).toString() + date.getDate().toString();
+ 
+// Compare date and create localStorage if it's not existing/too old   
+if (typeof storageFilesDate === "undefined" || storageFilesDate < todaysDate) {
+    // Take action when the image has loaded
+    imageID.addEventListener("load", function () {
+        var imgCanvas = document.createElement("canvas"),
+            imgContext = imgCanvas.getContext("2d");
+ 
+        // Make sure canvas is as big as the picture
+        imgCanvas.width = imageID.width;
+        imgCanvas.height = imageID.height;
+ 
+        // Draw image into canvas element
+        imgContext.drawImage(imageID, 0, 0, imageId.width, imageID.height);
+ 
+        // Save image as a data URL
+        storageFiles.imageID = imgCanvas.toDataURL("image/gif");
+ 
+        // Set date for localStorage
+        storageFiles.date = todaysDate;
+ 
+        // Save as JSON in localStorage
+        try {
+            localStorage.setItem("storageFiles", JSON.stringify(storageFiles));
+        }
+        catch (e) {
+            console.log("Storage failed: " + e);
+        }
+    }, false);
+ 
+    // Set initial image src    
+  //  imageID.setAttribute("src", "elephant.png");
 }
-
+else {
+    // Use image from localStorage
+   imageID.setAttribute("src", storageFiles.imageID);
+}
+	
+	
 //$('#feed').on('click','article',function(){
-
-
+	
+	
+	/* var imageId = document.getElementById("a"+[i]);
+	console.log(imageId);
+	
+	imageId.addEventListener("load", function (){
+		var imgCanvas = document.createElement("canvas"),
+		imgContext = imgCanvas.getContext("2d");
+		
+		imgCanvas.width = imageId.width;
+		imgCanvas.height=imageId.height;
+		
+		var imgAsDataURL = imgCanvas.toDataURL("image/gif");
+		
+		try{
+			localStorage.setItem("a"+[i], imgAsDataURL);
+		}
+		catch (e) {
+			console.log("Storage failed: " + e);	
+		}, false);
+		
+	}) */
+	
+	
+	
+	
